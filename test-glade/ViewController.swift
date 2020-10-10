73def13f1c39f36b20bb9e601e0acf4c7340bf39
allengu01 <allen.gu01@gmail.com>
2020-10-09 22:32:45 -0500

    Initial Commit

---
+++ test-glade/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test-glade
+//
+//  Created by Allen Gu on 10/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

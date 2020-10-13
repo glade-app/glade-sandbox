08fae675cc59af4dd2fa8342d34e4243e14e418f
allengu01 <allen.gu01@gmail.com>
2020-10-12 22:43:51 -0500

    Got the Spotify authentication working. Trying to understand how it works now

---
+++ test-glade/Constants.swift
@@ -0,0 +1,5 @@
+struct Constants {
+    static let clientID = "cb83fc0fc34a405a918e3ffda44dd2f1"
+    static let redirectURI = URL(string: "test-glade://")!
+    static let sessionKey = "spotifySessionKey"
+}
--- test-glade/SpotifyConnectViewController.swift
+++ test-glade/SpotifyConnectViewController.swift
@@ -7,17 +7,39 @@
 
 import UIKit
 
-class SpotifyConnectViewController: UIViewController {
+class SpotifyConnectViewController: UIViewController, SPTSessionManagerDelegate, SPTAppRemoteDelegate, SPTAppRemotePlayerStateDelegate {
 
+    lazy var configuration: SPTConfiguration = {
+        let configuration = SPTConfiguration(clientID: Constants.clientID, redirectURL: Constants.redirectURI)
+        //plays most recent song
+        //configuration.playURI = ""
+        
+        configuration.tokenSwapURL = URL(string: "http://localhost:1234/swap")
+        configuration.tokenRefreshURL = URL(string: "http://localhost:1234/refresh")
+        return configuration
+    }()
+
+    lazy var sessionManager: SPTSessionManager = {
+        let manager = SPTSessionManager(configuration: configuration, delegate: self)
+        return manager
+    }()
+
+    lazy var appRemote: SPTAppRemote = {
+        let appRemote = SPTAppRemote(configuration: configuration, logLevel: .debug)
+        appRemote.delegate = self
+        return appRemote
+    }()
+
+    private var lastPlayerState: SPTAppRemotePlayerState?
+    
+    
     var userData: [String: String] = [:]
     @IBOutlet var connectButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        
-        // Customized button
+        // Customized button move to custom view later
         connectButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 32)
         connectButton.setTitleColor(UIColor.white, for: .normal)
         connectButton?.layer.cornerRadius = (connectButton?.frame.size.height ?? 0)/2.0
@@ -28,6 +50,40 @@ class SpotifyConnectViewController: UIViewController {
     }
     
     @IBAction func connectButtonTapped(_ sender: Any) {
+        let scope: SPTScope = [.appRemoteControl, .playlistReadPrivate]
+
+        if #available(iOS 11, *) {
+            // Use this on iOS 11 and above to take advantage of SFAuthenticationSession
+            sessionManager.initiateSession(with: scope, options: .clientOnly)
+        } else {
+            // Use this on iOS versions < 11 to use SFSafariViewController
+            sessionManager.initiateSession(with: scope, options: .clientOnly, presenting: self)
+        }
+    }
+    
+    func sessionManager(manager: SPTSessionManager, didInitiate session: SPTSession) {
+        print("Connected")
+        appRemote.connect()
+    }
+    
+    func sessionManager(manager: SPTSessionManager, didFailWith error: Error) {
+        print("Authorization failed")
+    }
+    
+    func appRemoteDidEstablishConnection(_ appRemote: SPTAppRemote) {
+        print("Established connection")
+    }
+    
+    func appRemote(_ appRemote: SPTAppRemote, didFailConnectionAttemptWithError error: Error?) {
+        print("Failed connection attempt")
+    }
+    
+    func appRemote(_ appRemote: SPTAppRemote, didDisconnectWithError error: Error?) {
+        print("Disconnected with error")
+    }
+    
+    func playerStateDidChange(_ playerState: SPTAppRemotePlayerState) {
+        print("Player state changed")
     }
     
 }
